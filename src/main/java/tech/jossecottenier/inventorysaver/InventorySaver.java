3742596bce2eda4e62fdf52b4e03e5f5b0b670d7
Josse3 <josse.cottenier@hotmail.com>
2020-08-17 15:46:56 +0200

    Mark methods only for testing as protected

--- src/main/java/tech/jossecottenier/inventorysaver/InventorySaver.java
+++ src/main/java/tech/jossecottenier/inventorysaver/InventorySaver.java
@@ -1,26 +1,53 @@
 package tech.jossecottenier.inventorysaver;
 
-import java.util.List;
-
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class InventorySaver implements Listener {
 	
-	public static List<String> serializeInventory(Player player) {
+	public String serializeInventory(Player player) {
 		final PlayerInventory inventory = player.getInventory();
 		
 		return null;
 	}
 	
-	public static void saveInventory(Player player, JavaPlugin plugin) {
+	protected void saveInventory(Player player, JavaPlugin plugin) {
 		
 	}
 	
-	public static void saveInventory(Player player) {
+	public void saveInventory(Player player) {
 		saveInventory(player, Main.instance);
 	}
 	
+	protected ItemStack[] loadInventoryContents(Player player, JavaPlugin plugin) {
+		return null;
+	}
+	
+	public ItemStack[] loadInventoryContents(Player player) {
+		return loadInventoryContents(player, Main.instance);
+	}
+	
+	protected void onTeleport(PlayerTeleportEvent event, JavaPlugin plugin) {
+		
+	}
+	
+	@EventHandler
+	public void onTeleport(PlayerTeleportEvent event) {
+		onTeleport(event, Main.instance);
+	}
+	
+	protected void onQuit(PlayerQuitEvent event, JavaPlugin plugin) {
+		
+	}
+	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		onQuit(event, Main.instance);
+	}
 }
